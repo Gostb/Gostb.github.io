fdba0cca8f39e0db25488f429fc3f9bb2e8faba7
Maks <36163370+Gostb@users.noreply.github.com>
2020-07-25 02:46:35 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		delay: .3,
 		opacity: 1
 	});
+	TweenMax.to(".player", 1, {
+		delay: 8,
+		opacity: 0
+	});
 
 	// avatar appearing
 	TweenMax.fromTo(".player__avatar", .5, {
@@ -14,6 +18,16 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		opacity: 1
 	});
 
+	TweenMax.to(".player__avatar", .3, {
+		delay: 7.8,
+		opacity: 0
+	});
+
+	TweenMax.to(".player__img", .5, {
+		delay: 8.4,
+		width: 0
+	});
+
 	// nick appearing
 	TweenMax.fromTo(".player__nick", .7, {
 		width: "0px"
@@ -21,11 +35,22 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		width: "auto"
 	});
 
+	TweenMax.to(".player__nick", .7, {
+		delay: 8,
+		width: "0px",
+		padding: "4px 0"
+	});
+
 	TweenMax.fromTo(".player__nick-text", .3, {
 		yPercent: -100
 	}, {
 		delay: 1.2,
 		yPercent: 0
+	});
+
+	TweenMax.to(".player__nick-text", .2, {
+		delay: 7.8,
+		yPercent: -100
 	});
 
 	// social appearing
@@ -36,6 +61,12 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		width: "auto"
 	});
 
+	TweenMax.to(".player__socials", .7, {
+		delay: 8,
+		width: 0,
+		padding: 0
+	});
+
 	TweenMax.fromTo(".player__socials-icon", .2, {
 		opacity: 0
 	}, {
@@ -43,11 +74,21 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		opacity: 1
 	});
 
+	TweenMax.to(".player__socials-icon", .2, {
+		delay: 8,
+		opacity: 0
+	});
+
 	TweenMax.fromTo(".player__socials-text", .3, {
 		yPercent: 100
 	}, {
 		delay: 1.3,
 		yPercent: 0
+	});
+
+	TweenMax.to(".player__socials-text", .3, {
+		delay: 7.7,
+		yPercent: 100
 	});
 });
 
